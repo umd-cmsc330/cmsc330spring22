d356885fd5be60a07074f3c76768ba712fa5a1f0
Vyoma <vyomajani@gmail.com>
2022-02-10 11:05:50 -0500

    Adding disc 3 solutions

---
+++ discussions/d3_intro_ocaml/src/disc3_solutions.ml
@@ -0,0 +1,43 @@
+let positive n = if n > 0 then "positive" else if n < 0 then "negative" else "zero" 
+
+let double x = 2*x
+
+let fizz n= if n mod 15 = 0 then "fizz buzz" else if n mod 3 = 0 then "fizz" else if n mod 5 = 0 then "buzz" else ""
+
+let first_elem lst = match lst with 
+| h::t -> h
+| [] -> 0
+
+let rec sum lst = match lst with 
+| [] -> 0
+| h::t -> h+sum t
+
+let max_list lst = match lst with 
+| [] -> 0
+| h::t -> max h (max_list t)
+
+let sum_tuple t = match t with 
+| (a,b,c) -> a+b+c
+
+let rec product lst = match lst with 
+| [] -> 1
+| h::t -> h*product t in 
+
+let rec max_product lst = match lst with 
+| [] -> 0
+| h::t -> max (product h) (max_product t)
+
+let rec length l = match l with 
+| [] -> 0
+| h::t -> 1+length t in 
+
+let rec check_matrix_aux lst len = match l with 
+| [] -> true
+| h::t -> if length h = len then check_matrix_aux t s else false in 
+
+let check_matrix lst = match lst with 
+| h::t -> check_matrix_aux lst (length h)
+| [] -> true
+
+
+
